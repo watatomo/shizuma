7056c858e213ebe494899ffa63b47522bc169aaf
Ren <hodorugi@gmail.com>
2023-12-12 16:27:47 -0800

    added stuff...! yay

---
+++ src/main.js
@@ -0,0 +1,7 @@
+function showSettings() {
+    document.getElementById("settings").style.display = "flex";
+}
+
+function closeSettings() {
+    document.getElementById("settings").style.display = "none";
+}
