7cabbba54030f05d3755c031ecd25dd104febd4a
Ren <hodorugi@gmail.com>
2023-12-16 14:30:33 -0800

    add 12 hour clock

--- src/main.js
+++ src/main.js
@@ -14,6 +14,19 @@ function closeAddLink() {
     document.getElementById("addLink").style.display = "none";
 }
 
-function currentTime24() {
-    setInterval();
-}
+// add links
+
+// display current time
+window.onload = () => {
+    setInterval(() => {
+        const currentTime = new Date();
+        const hr = currentTime.getHours();
+        const min = currentTime.getMinutes();
+        const ampm = hr >= 12 ? "PM" : "AM";
+
+        // formats 12 hour clock
+        document.getElementById("clock").innerHTML = `${
+            hr === 12 ? 12 : hr % 12
+        }:${min} ${ampm}`;
+    }, 1000);
+};
